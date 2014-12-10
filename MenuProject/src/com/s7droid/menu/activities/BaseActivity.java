71f50894c33e571689388ec59c89e57c9af2c8b5
s7droid <droiddev@s7designcreative.com>
2014-12-10 20:12:27 +0100

    ActionBar changes.

--- MenuProject/src/com/s7droid/menu/activities/BaseActivity.java
+++ MenuProject/src/com/s7droid/menu/activities/BaseActivity.java
@@ -1,29 +1,13 @@
 package com.s7droid.menu.activities;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-
-import com.s7design.menu.R;
 
 public class BaseActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		ActionBar actionBar = getSupportActionBar();
-		actionBar.setDisplayHomeAsUpEnabled(true);
-		actionBar.setDisplayShowCustomEnabled(true);
-		actionBar.setDisplayShowTitleEnabled(false);
-
-		LayoutInflater inflator = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View v = inflator.inflate(R.layout.action_bar, null);
-
-		actionBar.setCustomView(v);
 
 	}
 
--- MenuProject/src/com/s7droid/menu/activities/MainActivity.java
+++ MenuProject/src/com/s7droid/menu/activities/MainActivity.java
@@ -1,16 +1,20 @@
 package com.s7droid.menu.activities;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 import com.s7design.menu.R;
 
-import android.app.Activity;
-import android.os.Bundle;
-
-
-public class MainActivity extends BaseActivity{
+public class MainActivity extends BaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Toolbar actionBar = (Toolbar) findViewById(R.id.actionBar);
+		actionBar.setTitle("");
+		setSupportActionBar(actionBar);
 	}
-	
+
 }
